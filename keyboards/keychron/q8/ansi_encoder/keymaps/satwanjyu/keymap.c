3b2286717117f95d7a1d6302cee3b19850730bdd
satwanjyu <satwanjyu@proton.me>
2024-08-10 09:30:08 +0000

    add fn for gaming layer

--- keyboards/keychron/q8/ansi_encoder/keymaps/satwanjyu/keymap.c
+++ keyboards/keychron/q8/ansi_encoder/keymaps/satwanjyu/keymap.c
@@ -24,7 +24,8 @@ enum layers {
     NUM,
     SYM,
     FUN,
-    GAME
+    GAME,
+    GFUN
 };
 
 bool dip_switch_update_user(uint8_t index, bool active) {
@@ -153,6 +154,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,  KC_LBRC,  KC_RBRC,  KC_BSLS,             KC_DEL,
    KC_CAPS,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                         KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,   KC_ENT,                      KC_HOME,
    KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,               KC_B,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  KC_RSFT,              KC_UP,  
-   KC_LCTL,  KC_LGUI,            KC_LALT,   KC_SPC,    KC_NO,              KC_NO,   KC_SPC,  KC_RALT,                                          KC_LEFT,  KC_DOWN,  KC_RGHT
+   KC_LCTL,  KC_LGUI,            KC_LALT,   KC_SPC,    KC_NO,           MO(GFUN),   KC_SPC,  KC_RALT,                                          KC_LEFT,  KC_DOWN,  KC_RGHT
+    ),
+  [GFUN] = LAYOUT_ansi_69(
+     KC_NO,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,              KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,    KC_NO,                       TD_MED,
+     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                        KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,              KC_NO,
+     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                        KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                        KC_NO,
+     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,              KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,              KC_NO,     
+     KC_NO,    KC_NO,              KC_NO,    KC_NO,    KC_NO,              KC_NO,    KC_NO,    KC_NO,                                            KC_NO,    KC_NO,    KC_NO
     ),
 };
